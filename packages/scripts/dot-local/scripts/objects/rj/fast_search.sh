5a4193a41e2125b6f9122acf39af6f9d5a203b1d
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2024-05-30 15:55:16 -0300

    Cleared scripts structure

--- packages/scripts/dot-local/scripts/objects/rj/fast_search.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-
-RJ_PATH=$HOME/.local/rj
-
-search_through=$(find $RJ_PATH -mindepth 1 -maxdepth 1 -type f | fzf)
-
--- packages/scripts/dot-local/scripts/objects/rj/note/new.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/bash
-
-VIM=$(which vim)
-
-sub_path="$1"
-
-# Change rj path
-cd $HOME/rj/$sub_path/ && $VIM ; cd -
-
