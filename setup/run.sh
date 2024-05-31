9d358c2efdcf16a77e7763227b82c3e10f8f115a
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2024-05-31 19:03:33 -0300

    Added vault role to full setup

--- setup/run.sh
+++ setup/run.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-ansible-playbook -i inventories/inv.yml ./full.yml --ask-become-pass
+echo -e 'Choose which play is going to run: \n1. Full setup\n2. Tooling only'
+read choice
+
+if [ "$choice" == '1' ]; then
+    ansible-playbook -i inventories/inv.yml ./full.yml --ask-become-pass --ask-vault-pass
+else
+    ansible-playbook -i inventories/inv.yml ./tooling_only.yml --ask-become-pass
+fi
+
 
