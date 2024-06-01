c0fa480028e4b2461afac808535f0a02fa7fb512
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2024-06-01 07:28:54 -0300

    Added setup path to run.sh to make it avaiable everywhere

--- setup/run.sh
+++ setup/run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
+SETUP_PATH=$HOME/.dotfiles/setup
+
 echo -e 'Choose which play is going to run: \n1. Full setup\n2. Tooling only'
 read choice
 
 if [ "$choice" == '1' ]; then
-    ansible-playbook -i inventories/inv.yml ./full.yml --ask-become-pass --ask-vault-pass
+    ansible-playbook -i $SETUP_PATH/inventories/inv.yml $SETUP_PATH/full.yml --ask-become-pass --ask-vault-pass
 else
-    ansible-playbook -i inventories/inv.yml ./tooling_only.yml --ask-become-pass
+    ansible-playbook -i $SETUP_PATH/inventories/inv.yml $SETUP_PATH/tooling_only.yml --ask-become-pass
 fi
 
 
