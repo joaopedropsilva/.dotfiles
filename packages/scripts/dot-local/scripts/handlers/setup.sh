23e8f22281234358027f94f5c4ce830bd8412d90
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2024-06-03 18:03:52 -0300

    Added launching setup test environment script to switch_actions

--- packages/scripts/dot-local/scripts/handlers/setup.sh
+++ packages/scripts/dot-local/scripts/handlers/setup.sh
@@ -1,23 +1,32 @@
-#!/usr/bin/env bash
+#! /usr/bin/bash
 
-TEST_DOCKER_IMAGE='arch_ansible'
+TEST_ENV_DOCKER_IMAGE='arch_ansible'
 SETUP_PATH="$HOME/.dotfiles/setup"
 ANSIBLE_PLAYBOOK=$(which ansible-playbook)
+DOCKER=$(which docker)
+BASH=$(which bash)
 
-rebuild_env() {
+rebuild_test_env() {
     echo '[rebuild setup test environment] Cleaning any running containers'
-    docker rm -f $TEST_DOCKER_IMAGE > /dev/null
+    docker rm -f $TEST_ENV_DOCKER_IMAGE > /dev/null
 
     echo '[rebuild setup test environment] Deleting past images'
-    docker image rm -f $TEST_DOCKER_IMAGE
+    docker image rm -f $TEST_ENV_DOCKER_IMAGE
 
-    echo "[rebuild setup test environment] Rebuilding $TEST_DOCKER_IMAGE image"
-    docker build $SETUP_PATH -t $TEST_DOCKER_IMAGE
+    echo "[rebuild setup test environment] Rebuilding $TEST_ENV_DOCKER_IMAGE image"
+    docker build $SETUP_PATH -t $TEST_ENV_DOCKER_IMAGE
+}
+
+launch_test_env() {
+    $DOCKER rm -f $TEST_ENV_DOCKER_IMAGE > /dev/null 2>&1
+    $DOCKER run --name=$TEST_ENV_DOCKER_IMAGE -it $TEST_ENV_DOCKER_IMAGE $BASH
 }
 
 if [ "$1" = 'Run' ]; then
     $ANSIBLE_PLAYBOOK -i $SETUP_PATH/inventories/inv.yml $SETUP_PATH/tooling_only.yml --ask-become-pass
 elif [ "$1" = 'Rebuild' ]; then
-    rebuild_env
+    rebuild_test_env
+elif [ "$1" = 'Launch' ]; then
+    launch_test_env
 fi
 
