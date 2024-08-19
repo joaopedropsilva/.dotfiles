5adb25ec0455b301e5df9bccd7a74f7fd93d9651
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2024-08-19 19:14:51 -0300

    Added zet backup script

--- packages/scripts/dot-local/scripts/switch_actions.sh
+++ packages/scripts/dot-local/scripts/switch_actions.sh
@@ -5,6 +5,8 @@ HANDLERS_PATH="$SCRIPTS_PATH/handlers"
 ACTIONS=$(cat "$SCRIPTS_PATH/actions")
 
 selected_action_string=($(echo "$ACTIONS" | fzf))
+
+# Grabs the last word of the sentence
 handler=$(echo "${selected_action_string[0]}" | sed 's/\[\(.*\)\]/\1/')
 
 if [ "$handler" = '' ]; then
